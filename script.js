c859c74df7cf715555d1fd696fd20ca186da74eb
orangegameube <rorysorola@gmail.com>
2026-07-03 23:00:34 -0500

    made an about me page, some of the image and text adaptation to window resizing is still a little off, but the window is completely functional (minus minimize function & taskbar)!

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ dragElement(document.getElementById("aboutwindow"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
+  if (document.getElementById(elmnt.id + "edge")) {
     // check to verify if present
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    document.getElementById(elmnt.id + "edge").onmousedown = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
