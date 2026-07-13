ee78bfd3a17d7fa2f5624bb0c1d4e33befad4106
orangegameube <rorysorola@gmail.com>
2026-07-13 10:34:13 -0500

    MIT CPW segment is done! im so tired of using copy and paste please save me

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ dragElement(document.getElementById("galleryapp"))
 dragElement(document.getElementById("yspa"))
 dragElement(document.getElementById("has"))
 dragElement(document.getElementById("flying"))
+dragElement(document.getElementById("cpw"))
 dragElement(document.getElementById("blogwindow"))
 
 
@@ -81,6 +82,7 @@ document.getElementById("galleryapp").style.visibility = "hidden";
 document.getElementById("yspa").style.visibility = "hidden";
 document.getElementById("has").style.visibility = "hidden";
 document.getElementById("flying").style.visibility = "hidden";
+document.getElementById("cpw").style.visibility = "hidden";
 document.getElementById("blogwindow").style.visibility = "hidden";
 
 function openTab(id) {
